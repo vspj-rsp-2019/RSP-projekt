d27f08900da710d402646bce83bf4048d8ca09af
Medlikova <56480688+Medlikova@users.noreply.github.com>
2019-11-20 23:07:49 +0100

    nové články
    - možnost změnit status článku

--- Implementace SW/RSP_projekt_autor/RSP_projekt_autor/NoveClanky.aspx.cs
+++ Implementace SW/RSP_projekt_autor/RSP_projekt_autor/NoveClanky.aspx.cs
@@ -31,15 +31,98 @@ public partial class NoveClanky : System.Web.UI.Page
 
     }
 
-    protected void HyperLink1_DataBinding(object sender, EventArgs e)
+   
+    //změna statusu na c.1 k formalnimu doplneni
+    protected void btn_zmenaStatusu_Click(object sender, EventArgs e)
     {
-        //GridView1.SelectedValue
-            
-        //    ([autor_id] = @autor_id)
-        //Clanky filePath
+        sql = "UPDATE Clanky SET status_clanek = 1 WHERE Id = @Id";
+
+           
+        try
+        {
+            con.Open();
+            sqlCmd = new SqlCommand(sql, con);
+
+            sqlCmd.Parameters.AddWithValue("@Id", GridView1.SelectedValue);
+           
+            sqlCmd.ExecuteNonQuery();
+
+            sqlCmd.Dispose();
+            con.Close();
+
+            lbl_stav.ForeColor = System.Drawing.ColorTranslator.FromHtml("#66FF33");
+            lbl_stav.Visible = true;
+            lbl_stav.Text = "Úspěšně uloženo.";
+            Response.Redirect(Request.RawUrl);
+
+
+
+        }
+        catch (Exception ex)
+        {
+            lbl_stav.Text = "Změna neproběhla.";
+           lbl_stav.Visible = true ;
+        }
+       
     }
 
-    protected void btn_zobraz_pdf_Click(object sender, EventArgs e)
+    //predat clanek do recenzniho rizeni
+    protected void btn_odeslat_do_RR_Click(object sender, EventArgs e)
     {
+        sql = "UPDATE Clanky SET status_clanek = 2 WHERE Id = @Id";
+
+
+        try
+        {
+            con.Open();
+            sqlCmd = new SqlCommand(sql, con);
+
+            sqlCmd.Parameters.AddWithValue("@Id", GridView1.SelectedValue);
+
+            sqlCmd.ExecuteNonQuery();
+
+            sqlCmd.Dispose();
+            con.Close();
+            lbl_stav.ForeColor = System.Drawing.ColorTranslator.FromHtml("#66FF33");
+            lbl_stav.Visible = true;
+            lbl_stav.Text = "Úspěšně uloženo.";
+            Response.Redirect(Request.RawUrl);
+
+        }
+        catch (Exception ex)
+        {
+            lbl_stav.Text = "Změna neproběhla.";
+            lbl_stav.Visible = true;
+        }
+    }
+    //Zamitnuti clanku
+    protected void btn_zamitnoutcl_Click(object sender, EventArgs e)
+    {
+        sql = "UPDATE Clanky SET status_clanek = 7 WHERE Id = @Id";
+
+
+        try
+        {
+            con.Open();
+            sqlCmd = new SqlCommand(sql, con);
+
+            sqlCmd.Parameters.AddWithValue("@Id", GridView1.SelectedValue);
+
+            sqlCmd.ExecuteNonQuery();
+
+            sqlCmd.Dispose();
+            con.Close();
+            lbl_stav.ForeColor = System.Drawing.ColorTranslator.FromHtml("#66FF33");
+            lbl_stav.Visible = true;
+            lbl_stav.Text = "Úspěšně uloženo.";
+            Response.Redirect(Request.RawUrl);
+                    
+
+        }
+        catch (Exception ex)
+        {
+            lbl_stav.Text = "Změna neproběhla.";
+            lbl_stav.Visible = true;
+        }
     }
 }
--- Implementace SW/RSP_projekt_autor/RSP_projekt_autor/ZamitnuteClanky.aspx
+++ Implementace SW/RSP_projekt_autor/RSP_projekt_autor/ZamitnuteClanky.aspx
@@ -4,8 +4,57 @@
     Zamítnuté články
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="head" Runat="Server">
+    <style type="text/css">
+        .auto-style5 {
+            margin-left: 7px;
+            margin-right: 7px;
+        }
+    </style>
 </asp:Content>
 <asp:Content ID="Content3" ContentPlaceHolderID="contentBody" Runat="Server">
-    Zamítnuté články
+
+    <h4 class="auto-style5">Zamítnuté články</h4>
+    <p class="auto-style5">&nbsp;</p>
+    <p class="auto-style5">
+        <asp:GridView ID="GridView1" runat="server" AllowPaging="True" AllowSorting="True" AutoGenerateColumns="False" CellPadding="4" DataKeyNames="Id" DataSourceID="SqlDataSource1" ForeColor="#333333" GridLines="None">
+            <AlternatingRowStyle BackColor="White" />
+            <Columns>
+                <asp:BoundField DataField="Id" HeaderText="Id" InsertVisible="False" ReadOnly="True" SortExpression="Id" />
+                <asp:BoundField DataField="Datum" HeaderText="Datum příspěvku" SortExpression="Datum" />
+                <asp:BoundField DataField="jmeno" HeaderText="Jméno autora" SortExpression="jmeno" />
+                <asp:BoundField DataField="prijmeni" HeaderText="Příjmení autora" SortExpression="prijmeni" />
+                <asp:BoundField DataField="Expr1" HeaderText="Status článku" SortExpression="Expr1" />
+            </Columns>
+            <FooterStyle BackColor="#990000" Font-Bold="True" ForeColor="White" />
+            <HeaderStyle BackColor="#990000" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#FFCC66" ForeColor="#333333" HorizontalAlign="Center" />
+            <RowStyle BackColor="#FFFBD6" ForeColor="#333333" />
+            <SelectedRowStyle BackColor="#FFCC66" Font-Bold="True" ForeColor="Navy" />
+            <SortedAscendingCellStyle BackColor="#FDF5AC" />
+            <SortedAscendingHeaderStyle BackColor="#4D0000" />
+            <SortedDescendingCellStyle BackColor="#FCF6C0" />
+            <SortedDescendingHeaderStyle BackColor="#820000" />
+        </asp:GridView>
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionString %>" SelectCommand="SELECT Clanky.Datum, Users.jmeno, Users.prijmeni, 
+Clanky.filePath, Clanky.datum_zah_rec_riz, Clanky.Id, 
+Clanky_Status.nazev AS Expr1 FROM Clanky 
+INNER JOIN Clanky_Status ON Clanky.status_clanek = Clanky_Status.Id_cl_status 
+INNER JOIN Users ON Clanky.autor_id = Users.id 
+WHERE (Clanky_Status.Id_cl_status = 7)"></asp:SqlDataSource>
+    </p>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 </asp:Content>
 
