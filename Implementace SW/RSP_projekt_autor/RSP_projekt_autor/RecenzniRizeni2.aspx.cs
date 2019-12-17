40846e449f1cbdd6ed3537a4978a4f33f81c1dc3
Medlikova <56480688+Medlikova@users.noreply.github.com>
2019-12-17 15:15:40 +0100

    Vytvořena možnost pro zahájení Recenzního řízení:
    - nová stránka RecenzniRizeni2.aspx, odstraněna stránka RecenzniRizeni.aspx
    - umožňuje zvolení zahájení RR s volbou datumu.
    - zobrazení Zahájených RR bez přiřazených Recenzentů
    - zobrazení probíhajících RR

---
+++ Implementace SW/RSP_projekt_autor/RSP_projekt_autor/RecenzniRizeni2.aspx.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+public partial class RecenzniRizeni2 : System.Web.UI.Page
+{
+    // Vytvori pripojeni k databazi 
+    SqlCommand cmd = new SqlCommand();
+    SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+    SqlCommand sqlCmd;
+    string sql = null;
+    User user;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        user = (User)Session["userObject"];
+        if (user == null)
+        {
+            Response.Redirect("login.aspx");
+        }
+    }
+    /*
+     * Nastavi hodnotu vybranou v kalendari do pole (labelu) pro datum 
+     */
+
+    protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+    {
+        lbl_zahRR.Text = Calendar1.SelectedDate.ToShortDateString();
+    }
+
+    protected void btn_odeslatDoZahajeni_Click(object sender, EventArgs e)
+    {
+        if ((GridView1.SelectedIndex >= 0) && (lbl_zahRR.Text.Length > 1))
+        {
+            if (DateTime.Now.Date > Calendar1.SelectedDate.Date)
+            {
+                lbl_zahRR.Text = "Vybrané datum je menší než aktuální!";
+            }
+            else
+            {
+               // aktualizuje tabulku články, přiřadí stav článku na zahájení RR, a vloží datum dle vybraného z kalendáře
+               sql = "UPDATE Clanky SET status_clanek = 3, datum_zah_rec_riz = @datumZahajeniRR WHERE Id = @Id";
+                
+                try
+                {
+                    con.Open();
+                    sqlCmd = new SqlCommand(sql, con);
+
+                    sqlCmd.Parameters.AddWithValue("@Id", GridView1.SelectedValue);
+                   
+                    sqlCmd.Parameters.AddWithValue("@datumZahajeniRR", Calendar1.SelectedDate.ToString("MM/dd/yyyy"));
+
+                    sqlCmd.ExecuteNonQuery();
+
+                    sqlCmd.Dispose();
+                    con.Close();
+                    lbl_potvrzeni.Text = "Recenze přiřazena.";
+                   
+                }
+                catch (Exception ex)
+                {
+                    
+                    lbl_zahRR.Text = "Nepodařilo se přiřadit!";
+                }
+            }
+        }
+        else
+        {
+            lbl_zahRR.Text = "Některá z hodnot nebyla vybrána!";
+        }
+   
+    
+    }
+}
